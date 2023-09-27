95fae3621997a477b721eb15a4a2be7428e8920b
Priscila <priscila.campoos@yahoo.com.br>
2023-09-27 15:22:02 +0100

    Project setup

---
+++ src/test/java/com/prisc/hpcharactersage/HpCharactersAgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prisc.hpcharactersage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpCharactersAgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
