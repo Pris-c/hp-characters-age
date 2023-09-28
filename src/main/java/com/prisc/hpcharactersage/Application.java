307ff15f3809e87bbfb30f9dceb7b41934153291
Priscila <priscila.campoos@yahoo.com.br>
2023-09-28 17:23:34 +0100

    Added @OpenAPIDefinition annotation

--- src/main/java/com/prisc/hpcharactersage/Application.java
+++ src/main/java/com/prisc/hpcharactersage/Application.java
@@ -1,10 +1,13 @@
 package com.prisc.hpcharactersage;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+@OpenAPIDefinition(servers = {@Server(url="/", description = "Default server url")})
 @EnableFeignClients
 @SpringBootApplication
 public class Application {
